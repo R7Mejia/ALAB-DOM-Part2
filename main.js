9e1f7c0330bcff77511f2641b42a60180b1382e3
Roberto Mejia <robertomejiar7@gmail.com>
2023-10-26 16:10:31 -0400

    forth cimmit

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Menu data structure
 const menuLinks = [
-  { text: "about", href: "#" },
+  {
+    text: "about",
+    href: "#",
+  },
   {
     text: "catalog",
     href: "#",
@@ -36,7 +39,7 @@ const h1Element = document.createElement("h1");
 h1Element.textContent = "DOM Manipulation";
 mainEl.appendChild(h1Element);
 h1Element.style.color = "red";
-mainEl.style.backgroundColor = "#4a4e4d";
+mainEl.style.backgroundColor = "gray";
 
 // Step 2: Style the top menu
 const topMenuEl = document.querySelector("#top-menu");
@@ -44,29 +47,30 @@ topMenuEl.style.backgroundColor = "#0e9aa7";
 topMenuEl.classList.add("flex-around");
 
 //iterate over the entire menuLinks array and for each link create an <a> element that connects to the arrays buttons/hrefs
-      menuLinks.forEach(function (link) {
-        const anchorTag = document.createElement("a"); //creates an anchor (<a>)  tag
-        //link the arrays/href's
-        anchorTag.textContent = link.text;
-        anchorTag.href = link.href;
-        anchorTag.style.color = "yellow"; //Just added color. Mamma mia, ragazzi è una cosa bellissima!
-        anchorTag.classList.add("top-link"); //
-        //the anchorTag element will be displayed as a child of the topMenu element
-        topMenuEl.appendChild(anchorTag);
-      });
+menuLinks.forEach(function (link) {
+  const anchorTag = document.createElement("a"); //creates an anchor (<a>)  tag
+  //link the arrays/href's
+  anchorTag.textContent = link.text;
+  anchorTag.href = link.href;
+  anchorTag.style.color = "yellow"; //Just added color. Mamma mia, ragazzi è una cosa bellissima!
+  anchorTag.classList.add("top-link"); //
+  //the anchorTag element will be displayed as a child of the topMenu element
+  topMenuEl.appendChild(anchorTag);
+});
 
 // Step 3: Create the sub-menu and set its properties
 const subMenuEl = document.createElement("nav");
 subMenuEl.setAttribute("id", "sub-menu");
-subMenuEl.style.height = "100%";
+subMenuEl.style.height = "";
+//how can I make the subMenu bar dissapear after 2 seconds?
 subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 subMenuEl.classList.add("flex-around");
 document.querySelector("header").appendChild(subMenuEl);
 
-
-// Step 4: Handle click events on the top menu
+// Step 4:
 const topMenuLinks = document.querySelectorAll(".top-link");
 topMenuEl.addEventListener("click", mouseClick);
+// Showthe submenu.
 
 function mouseClick(evt) {
   evt.preventDefault();
@@ -74,20 +78,46 @@ function mouseClick(evt) {
     return;
   }
 
-  topMenuLinks.forEach(function (activeLink) {
-    activeLink.classList.remove("active");
-  });
-  evt.target.classList.add("active");
+  const clickedLink = evt.target;
 
-  const linkContent = evt.target.textContent;
-  console.log(linkContent);
+  // resolve the ABOUT issue
+  if (clickedLink.textContent === "about") {
+    //console.log(linkContent)
+  } else {
+    subMenuEl.style.height = "100%";
+    // Remove the "active" class from all links.
+    topMenuLinks.forEach((link) => link.classList.remove("active"));
+  }
+  // Add the "active" class to the clicked link.
+  clickedLink.classList.add("active");
 
-  menuLinks.forEach((l) => {
-    if (l.text === linkContent.toLowerCase()) {
-      subMenuEl.innerHTML = "";
-      
-    }
-  });
+  // Get the text of the clicked link.
+  const linkContent = clickedLink.textContent;
 
-  subMenuEl.style.top = "100%";
+  // Build the submenu.
+  buildSubmenu(linkContent);
+
+}
+function buildSubmenu(linkContent) {
+  // eliminating? the current contents of the subMenuEl element.
+  subMenuEl.innerHTML = "";
+
+  // Get the sub-links for the clicked link.
+  const subLinks = menuLinks.find((link) => link.text === linkContent).subLinks;
+  // console.log(subLinks)
+  // Append the sub-links to the subMenuEl element.
+  //console.log(subLinks)
+
+  if (subLinks) {
+    subLinks.forEach((subLink) => {
+      const anchorTag = document.createElement("a");
+      anchorTag.href = subLink.href;
+      anchorTag.textContent = subLink.text;
+      subMenuEl.appendChild(anchorTag);
+    });
+  }
+  // Hide the submenu after 2 seconds
+  setTimeout(() => {
+    subMenuEl.style.height = "0";
+  }, 5000);
 }
